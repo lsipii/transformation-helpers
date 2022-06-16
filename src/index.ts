d83fb05d776aee47d86842ad4e2e77e83793cb6c
Lassi Piironen <lsipii@kapsi.fi>
2022-06-16 13:51:51 +0300

    build: packaging

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * as Arrays from "./helpers/Arrays";
+export * as Dates from "./helpers/Dates";
+export * as Hashes from "./helpers/Hashes";
+export * as Objects from "./helpers/Objects";
+export * as Strings from "./helpers/Strings";
+export * as Transformations from "./helpers/Transformations";
